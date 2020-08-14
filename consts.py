848d87453f7ac68e9d7b48b092899a2873ebad44
Mehran Kholdi <semekh@hamravesh.com>
2020-08-15 01:24:55 +0430

    Change default provisioner name from `rawfile.hamravesh.com` to `rawfile.csi.openebs.io`

--- consts.py
+++ consts.py
@@ -1,3 +1,6 @@
-PROVISIONER_VERSION = "0.3.0"
+import os
+
+PROVISIONER_NAME = os.getenv("PROVISIONER_NAME", "rawfile.csi.openebs.io")
+PROVISIONER_VERSION = "0.4.0"
 DATA_DIR = "/data"
 CONFIG = {}
--- rawfile_servicer.py
+++ rawfile_servicer.py
@@ -4,7 +4,7 @@ import grpc
 from google.protobuf.wrappers_pb2 import BoolValue
 
 import rawfile_util
-from consts import PROVISIONER_VERSION
+from consts import PROVISIONER_VERSION, PROVISIONER_NAME
 from csi import csi_pb2, csi_pb2_grpc
 from declarative import be_mounted, be_unmounted, be_symlink, be_absent
 from metrics import volume_stats
@@ -20,7 +20,7 @@ class RawFileIdentityServicer(csi_pb2_grpc.IdentityServicer):
     @log_grpc_request
     def GetPluginInfo(self, request, context):
         return csi_pb2.GetPluginInfoResponse(
-            name="rawfile.hamravesh.com", vendor_version=PROVISIONER_VERSION
+            name=PROVISIONER_NAME, vendor_version=PROVISIONER_VERSION
         )
 
     @log_grpc_request
