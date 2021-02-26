d203eba5a93e0282603953060a7d1f8f10677990
Mehran Kholdi <semekh@hamravesh.com>
2021-02-26 17:56:47 +0330

    Release 0.4.4

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 import os
 
 PROVISIONER_NAME = os.getenv("PROVISIONER_NAME", "rawfile.csi.openebs.io")
-PROVISIONER_VERSION = "0.4.3"
+PROVISIONER_VERSION = "0.4.4"
 DATA_DIR = "/data"
 CONFIG = {}
