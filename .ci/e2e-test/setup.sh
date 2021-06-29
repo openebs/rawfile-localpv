c454a51ccda184f68a8c4429398c7a2f9e44ff85
Mehran Kholdi <semekh@hamravesh.com>
2021-06-29 15:02:26 +0430

    Nit: cleanup e2e test scripts

--- .ci/e2e-test/setup.sh
+++ .ci/e2e-test/setup.sh
@@ -2,14 +2,15 @@
 set -ex
 source .ci/common
 
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.1/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+K8S_VERSION=1.18.20
+MINIKUBE_VERSION=1.18.1
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 sudo apt update && sudo apt install -y conntrack
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.8.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 mkdir -p $HOME/.kube $HOME/.minikube
 touch $KUBECONFIG
-sudo minikube start --profile=minikube --vm-driver=none --kubernetes-version=v1.18.1
+sudo minikube start --profile=minikube --vm-driver=none --kubernetes-version=v${K8S_VERSION}
 minikube update-context --profile=minikube
-chown -R $(id -un):$(id -gn) ${HOME}/.minikube/
 eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
 
 curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
