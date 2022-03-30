3db48a0fd26a7e9d730a6c1e6396913b7afb6340
Mehran Kholdi <semekh@hamravesh.com>
2022-03-30 13:20:57 +0430

    Release 0.8.0

--- consts.py
+++ consts.py
@@ -1,7 +1,7 @@
 import os
 
 PROVISIONER_NAME = os.getenv("PROVISIONER_NAME", "rawfile.csi.openebs.io")
-PROVISIONER_VERSION = "0.7.0"
+PROVISIONER_VERSION = "0.8.0"
 DATA_DIR = "/data"
 CONFIG = {}
 RESOURCE_EXHAUSTED_EXIT_CODE = 101
