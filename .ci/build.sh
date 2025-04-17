86f5a1fd66b5cd5c3defa7d6facf5088cef82748
Muhammed Hussein Karimi <info@karimi.dev>
2025-04-17 18:22:27 +0330

    :recycle: Refactor build script to publish image
    improves multi-arch builds and remove usage of local registry
    
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,24 +5,37 @@ SCRIPT_DIR="$(dirname "$0")"
 set -exuo pipefail
 source "$SCRIPT_DIR/common"
 
-export IMAGE_DESTINATION="registry"
+command -v docker &>/dev/null || ( echo 'Docker is not installed. Aborting.'; exit 1 )
+docker buildx version &>/dev/null || ( echo 'Docker Buildx plugin is not installed. Aborting.'; exit 1 )
+
+export PUSH_OPTION=""
+export IMAGE_TAGS="${COMMIT}"
+
 if [ "${CI_IMAGE_PLATFORMS}" = "local" ]; then
   export IMAGE_DESTINATION="docker"
+else
+  export IMAGE_DESTINATION="registry"
+  if [ -n "${DNAME:-}" ] && [ -n "${DPASS:-}" ]; then
+    docker login -u "${DNAME}" -p "${DPASS}"
+    export PUSH_OPTION="--push"
+  fi
+  export IMAGE_TAGS="${COMMIT} ${BRANCH_SLUG}";
 fi
 
+for t in ${IMAGE_TAGS}; do
+  TAGS_ARGS+=" -t ${CI_IMAGE_NAME}:${t}${TAG_SUFFIX:-}"
+done
+
 docker buildx build \
-  --pull --no-cache \
+  --pull --no-cache ${PUSH_OPTION} \
+  ${TAGS_ARGS} \
   --output=type=${IMAGE_DESTINATION} \
   --platform="${CI_IMAGE_PLATFORMS}" \
-  -t "$CI_IMAGE_URI" \
   --build-arg "IMAGE_REPOSITORY=${IMAGE}" \
-  --build-arg "IMAGE_TAG=${COMMIT}" "$SCRIPT_DIR/.."
+  --build-arg "IMAGE_TAG=${COMMIT}" \
+  "$SCRIPT_DIR/.."
 
 if [ "${CI_IMAGE_PLATFORMS}" != "local" ]; then
   docker pull "$CI_IMAGE_URI"
 fi
 
-if [ -n "${DNAME:-}" ] && [ -n "${DPASS:-}" ]; then
-  docker login -u "${DNAME}" -p "${DPASS}";
-  TagAndPushImage $CI_IMAGE_REPO $CI_TAG;
-fi
